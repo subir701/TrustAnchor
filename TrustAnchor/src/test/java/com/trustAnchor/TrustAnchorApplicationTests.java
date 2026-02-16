5a1bda9e8b67efd81e2311c2baa3972a24f603b2
Subir Adhikari <subiradhikari181@gmail.com>
2026-02-17 00:25:08 +0530

    Project Structure and Dependencies are added

---
+++ TrustAnchor/src/test/java/com/trustAnchor/TrustAnchorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trustAnchor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustAnchorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
